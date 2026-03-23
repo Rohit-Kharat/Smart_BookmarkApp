5b5d87235c079b522b58c6e8e1f35cc285f918df
Rohit Kharat <kharatrohit1130@gmail.com>
2026-03-23 19:24:04 +0530

    Clean up AuthVantaLayer by removing comment
    Removed unnecessary comment in AuthVantaLayer component.

--- src/components/AuthVantaLayer.tsx
+++ src/components/AuthVantaLayer.tsx
@@ -4,7 +4,7 @@ import VantaBackground from "@/components/VantaBackground";
 import VantaBackgroundOfBookMark from "@/components/VantaBackgroundOfBookMark";
 
 export default function AuthVantaLayer({ isLoggedIn }: { isLoggedIn: boolean }) {
-  // key forces full remount -> guarantees Vanta re-inits
+  
   return isLoggedIn ? (
     <VantaBackgroundOfBookMark key="logged-in" />
   ) : (
